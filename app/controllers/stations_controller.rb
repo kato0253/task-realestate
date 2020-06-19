d96cd0a2b42f78355f5f86da3a1184b419a08110
kato <user@MacBook-Air.local>
2020-06-19 18:24:44 +0900

    changes_model

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,2 +1,15 @@
 class StationsController < ApplicationController
+  def new
+    @station = Station.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,8 @@
 class Property < ApplicationRecord
+    # validates :property_name, presence: true
+    # validates :price, presence: true
+    # validates :address, presence: true
+    # validates :age, presence: true
+    has_many :stations, dependent: :destroy, inverse_of: :property
+    accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
 end
